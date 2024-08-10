37aff08a084ef10af7916fa3b519d117943b19fe
aten040791 <aten040791@gmail.com>
2024-08-11 00:59:06 +0700

    Dashboard

---
+++ dcompose-stack/radar-cp-hadoop-stack/init.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env sh
+set -e
+
+echo '==' && echo "==> Enviroment variables"
+    echo PROJ=${PROJ}
+    echo PROJ_FOLDER=${PROJ_FOLDER}
+
+    API_URI=${API_URI:-'https://radar-backend.co.uk/api'}
+    echo API_URI=${API_URI}
+
+    BASE_HREF=${BASE_HREF:-'/'}
+    echo BASE_HREF=${BASE_HREF}
+
+echo '==' && echo "==> Replace API_URI & BASE_HREF"
+    cd /var/www
+
+    # file to replace
+    # FILE=$(find . -name 'main.*.js')
+
+    # regex patterns for API_URI
+    # FIND="(PARAMS\:\{[^\}]*)(API_URI\:[^\"]*\")([^\"]*)"
+    # REPLACE="\\1\\2${API_URI}"
+    
+    # replace API_URI value
+    # sed -ri "s|${FIND}|${REPLACE}|" ${FILE}
+    
+    # FIND="https://radar-cns-platform.rosalind.kcl.ac.uk"
+    #REPLACE="http://localhost"
+    #sed -ri "s|${FIND}|${REPLACE}|" ${FILE}
+
+    # file to replace
+    FILE="index.html"
+
+    # regex patterns for BASE_HREF
+    FIND="(\<base\ href\=\")([^\"]+)"
+    REPLACE="\\1${BASE_HREF}"
+
+    # replace BASE_HREF value
+    sed -ri "s|${FIND}|${REPLACE}|" ${FILE}
+
+echo '==' && echo "==> Starting nginx in the foreground"
+    nginx -g "daemon off;"
+
+exec "$@"
